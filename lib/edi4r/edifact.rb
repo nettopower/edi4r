41cf5be48235419cff7ef2d56cae0e32f97e87b5
Lars Hoeg <larshoeg@gmail.com>
2013-04-29 12:37:53 +0200

    fix error when seg is empty (ENDK has a bug which does this)

--- lib/edi4r/edifact.rb
+++ lib/edi4r/edifact.rb
@@ -119,7 +119,7 @@ module EDI::E
     #
     results << item unless item.empty?
     return results if results.empty?
-    while results.last.empty?; results.pop; end
+    while results.any? && results.last.empty?; results.pop; end
     results
   end
 
